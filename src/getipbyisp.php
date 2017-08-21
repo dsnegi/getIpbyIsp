33cf2b781a2c7da80c71d457a28a29685e810f15
hIMEI <himei@tuta.io>
2017-08-21 10:37:49 -0400

    country code validation added

--- src/getipbyisp.php
+++ src/getipbyisp.php
@@ -39,6 +39,32 @@ class GetIpCli
 {
     private $parser;
 
+    private $country_codes = array(
+                                'AD','AE','AF','AG','AI','AL','AM','AO','AQ','AR','AS',
+                                'AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG',
+                                'BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT',
+                                'BV','BW','BY','BZ','CA','CC','CD','CF','CG','CH','CI',
+                                'CK','CL','CM','CN','CO','CR','CU','CV','CW','CX','CY',
+                                'CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH',
+                                'ER','ES','ET','FI','FJ','FK','FM','FO','FR','GA','GB',
+                                'GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ',
+                                'GR','GS','GT','GU','GW','GY','HK','HM','HN','HR','HT',
+                                'HU','ID','IE','IL','IM','IN','IO','IQ','IR','IS','IT',
+                                'JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KP',
+                                'KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS',
+                                'LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MH',
+                                'MK','ML','MM','MN','MO','MP','MQ','MR','MS','MT','MU',
+                                'MV','MW','MX','MY','MZ','NA','NC','NE','NF','NG','NI',
+                                'NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG',
+                                'PH','PK','PL','PM','PN','PR','PS','PT','PW','PY','QA',
+                                'RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG',
+                                'SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST',
+                                'SV','SX','SY','SZ','TC','TD','TF','TG','TH','TJ','TK',
+                                'TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG',
+                                'UM','US','UY','UZ','VA','VC','VE','VG','VI','VN','VU',
+                                'WF','WS','YE','YT','ZA','ZM','ZW'
+                                );
+
     public function __construct()
     {
         $parser = new Console_CommandLine(array(
@@ -78,12 +104,19 @@ for city - its name, for ISP - single IP or ISP url\n".RESET
     {
         return $this->parser;
     }
+
+    public function getCountryCodes()
+    {
+        return $this->country_codes;
+    }
   
     public function cliParse()
     {
         $params = array();
 
         $parser = $this->getParser();
+
+        $country_codes = $this->getCountryCodes();
 
         try {
             $parsed = $parser->parse();
@@ -111,6 +144,14 @@ for city - its name, for ISP - single IP or ISP url\n".RESET
                 die(RED.BOLD."Invalid country name! Only 2 english letters must be there.\n".RESET);
             }
 
+            if ($parsed->args['type'] === 'country') {
+                foreach ($country_codes as $code) {
+                    if (($parsed->args['request']) !== $code) {
+                        die(RED.BOLD."Invalid country code! Here is no such country in our world.\n".RESET);
+                    }
+                }
+            }
+            
             $params['type']    = trim($parsed->args['type']);
             $params['url'] = trim($parsed->args['request']);
             $params['action']  = 'Отправить';
